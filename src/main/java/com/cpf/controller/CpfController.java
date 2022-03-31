c6d9638ec567d0d6f1326aad5def7c787bf5231e
Bruno Marques Gurgel <brunogurgel@Brunos-MacBook-Air.local>
2022-03-31 18:51:06 -0300

    fix

--- src/main/java/com/cpf/controller/CpfController.java
+++ src/main/java/com/cpf/controller/CpfController.java
@@ -1,6 +1,5 @@
 package com.cpf.controller;
 
-import netscape.javascript.JSObject;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
